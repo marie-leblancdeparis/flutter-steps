7e7d88521440107e0b78968b661e615c40222743
Alain Lompo <alain.lompo@gmail.com>
2019-10-19 17:35:37 +0200

    feat: Persistence and List, labs partially completed

--- spend_tracker/lib/application.dart
+++ spend_tracker/lib/application.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:spend_tracker/database/db_provider.dart';
 import 'package:spend_tracker/routes.dart';
-
-
+import 'package:provider/provider.dart';
 
 class Application extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Spend Tracker',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-       ),
-       initialRoute: '/',
-       routes: routes, 
-        );
+    return Provider<DbProvider>(
+      builder: (_) => DbProvider(),
+      dispose: (_, value) => value.dispose(),
+
+      child: MaterialApp(
+        title: 'Spend Tracker',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+         ),
+         initialRoute: '/',
+         routes: routes, 
+          ),
+    );
   }
 }
--- spend_tracker/lib/pages/accounts/account_page.dart
+++ spend_tracker/lib/pages/accounts/account_page.dart
@@ -1,29 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:spend_tracker/database/db_provider.dart';
+import 'package:spend_tracker/models/account.dart';
 import 'package:spend_tracker/pages/icons/icon_holder.dart';
+import 'package:spend_tracker/support/icon_helper.dart';
 
 class AccountPage extends StatefulWidget {
   
+  final Account account;
+  
+  AccountPage({this.account});
 
   @override
   _AccountPageState createState() => _AccountPageState();
 }
 
 class _AccountPageState extends State<AccountPage> {
-  Map<String, dynamic> _data = Map<String, dynamic>();
+  Map<String, dynamic> _data;
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  IconData _newIcon;
+  
+  @override
+  void initState() {
+    super.initState();
+    if (widget.account != null) {
+      _data = widget.account.toMap();
+    } else {
+      _data = Map<String, dynamic>();
+      _data['codePoint'] = Icons.add.codePoint;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    var dbProvider = Provider.of<DbProvider>(context);
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Account'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.save),
-            onPressed: () {
+            onPressed: () async {
               if(!_formKey.currentState.validate()) return;
               _formKey.currentState.save();
+              var account = Account.fromMap(_data);
+              if (account.id == null) {
+                await dbProvider.createAccount(account);
+              } else {
+                await dbProvider.updateAccount(account);
+              }
               Navigator.of(context).pop();
             },
            )
@@ -36,14 +61,15 @@ class _AccountPageState extends State<AccountPage> {
             child: Column(
               children: <Widget>[
                 IconHolder(
-                  newIcon: _newIcon,
+                  newIcon: IconHelper.createIconData(_data['codePoint']),
                   onIconChange: (IconData iconData) {
                     setState((){
-                      _newIcon = iconData;
+                      _data['codePoint'] = iconData.codePoint;
                     });
                   },
                   ),
                  TextFormField(
+                   initialValue: widget.account != null? widget.account.name: '',
                    decoration: InputDecoration(
                      labelText: 'Name',
                      ),
@@ -54,6 +80,7 @@ class _AccountPageState extends State<AccountPage> {
                      onSaved: (String value) => _data['name'] = value,
                   ),
                   TextFormField(
+                    initialValue: widget.account != null? widget.account.balance.toString(): '',
                     keyboardType: TextInputType.numberWithOptions(
                       decimal: true,
                      ),
